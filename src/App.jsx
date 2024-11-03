6e4564cc1c9c3f16088ae169c959dfc6f712c9a3
Murat Çolak <mo939614@gmail.com>
2024-11-03 19:26:01 +0300

    Edit Folderdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import userData from "./data/userData.json";
-import friendsData from "./data/friendData.json";
-import transaction from "./data/transaction.json";
+import UserData from "./data/UserData.json";
+import FriendsData from "./data/FriendData.json";
+import Transaction from "./data/Transaction.json";
 
 import Profile from "./components/ProfileFolder/Profile.jsx";
 import FriendList from "./components/FriendListFolder/FriendList.jsx";
 import TransactionHistory from "./components/TransactionHistoryFolder/TransactionHistory.jsx";
 
 const App = ({ initialUserIndex = 0 }) => {
-  const user = userData[initialUserIndex];
+  const user = UserData[initialUserIndex];
   return (
     <>
       {/* Profile Section */}
@@ -24,12 +24,12 @@ const App = ({ initialUserIndex = 0 }) => {
 
       {/* Friends Section */}
       <div id="friends-section">
-        <FriendList friends={friendsData} />
+        <FriendList friends={FriendsData} />
       </div>
 
       {/* Transactions Section */}
       <div id="history-section">
-        <TransactionHistory items={transaction} />
+        <TransactionHistory items={Transaction} />
       </div>
     </>
   );
