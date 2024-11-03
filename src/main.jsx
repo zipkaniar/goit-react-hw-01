6383e65c2759b2018f7bd0296ceb0ea14a902fbf
Murat Çolak <mo939614@gmail.com>
2024-11-03 20:38:43 +0300

    edit last hit ? main eror not jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import React from 'react'
 import App from './App.jsx'
+import React from 'react'
+
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+  <React.StrictMode>
     <App />
-    </React.StrictMode>
+  </React.StrictMode>
 )
