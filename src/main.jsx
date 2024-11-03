13c99297c6ece3c35c59f54666ac726c1d7e6732
Murat Çolak <mo939614@gmail.com>
2024-11-03 20:31:54 +0300

    main edit.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import React from 'react'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+  <StrictMode>
     <App />
-    </React.StrictMode>
+  </StrictMode>,
 )
